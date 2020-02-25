3054cc5e766cdfae4a5fbbbab6d42285e17cd9df
Runolf <alexandreliskiewicz@hotmail.com>
2020-02-25 22:53:48 +0100

    Add DB dump and a guitar image

---
+++ DB_InstruMagasin.sql
@@ -0,0 +1,59 @@
+CREATE TABLE Roles(
+	roleId INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+	`name` VARCHAR(50) 
+)ENGINE=INNODB;
+
+INSERT INTO Roles(`name`) VALUES("admin");
+INSERT INTO Roles(`name`) VALUES("user");
+
+CREATE TABLE Users(
+	userId INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+	email VARCHAR(50) NOT NULL,
+	pseudo VARCHAR(50) NOT NULL,
+	pswd VARCHAR(50) NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	street VARCHAR(50) NOT NULL,
+	number VARCHAR(50) NOT NULL,
+	rating DECIMAL(5,2),
+	RoleId INT,
+	FOREIGN KEY (RoleId) REFERENCES Roles(RoleId)
+)ENGINE=INNODB;
+
+INSERT INTO Users(email, pseudo, pswd, city, street, number, rating, RoleId)
+	   VALUES("alexandreliskiewicz@hotmail.com", "Runolf", "Test1234","Le Roeulx", "Chaussée de Soignies", "54", 100, 1);
+	  
+-- select u.pseudo, r.name as 'role'
+-- from Users as u
+-- join Roles as r
+-- on u.RoleId = r.RoleId;
+
+CREATE TABLE Articles(
+	articleId INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+	`name` VARCHAR(50), 
+	brand VARCHAR(50),
+	picture VARCHAR(255),
+	price DECIMAL(5,2),
+	`comment` VARCHAR(255) 
+)ENGINE=INNODB;
+
+
+
+INSERT INTO Articles(`name`, brand, picture, price, `comment`)
+		VALUES("cort zenox", "cort", "C:\Users\alexa\Documents\projetdev\Sell_Instrument_ProjectWeb\project\img\cort_zenox.jpg", 300.99, "amazing guitar");
+		
+
+CREATE TABLE Carts(
+	cartId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	articleId INT,
+	userId INT,
+	FOREIGN KEY(articleId) REFERENCES Articles(articleId),
+	FOREIGN KEY(userId) REFERENCES Users(userId)
+)ENGINE=INNODB;
+
+CREATE TABLE sellArticles(
+	sellArticleId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	articleId INT,
+	userId INT,
+	FOREIGN KEY(articleId) REFERENCES Articles(articleId),
+	FOREIGN KEY(userId) REFERENCES Users(userId)
+)ENGINE=INNODB;
