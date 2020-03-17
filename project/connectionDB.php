b0df24ba52086d5063bff671faf2010086e498d2
Runolf <alexandreliskiewicz@hotmail.com>
2020-03-17 16:30:20 +0100

    add class user

--- project/connectionDB.php
+++ project/connectionDB.php
@@ -3,7 +3,4 @@
     function getDB(){
       return new PDO('mysql:host=localhost;dbname=instrumagasin;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
-
-
-
 ?>
--- project/models/users.php
+++ project/models/users.php
@@ -2,7 +2,46 @@
 require 'connectionDB.php';
 
 class User{
-  
+  public $useId;
+  public $email;
+  public $pseudo;
+  public $pswd;
+  public $city;
+  public $street;
+  public $number;
+  public $rating;
+  public $RoleId;
+
+  public function __construct($data=null){
+    if(is_array($data)){
+      $this->$useId = $data["useId"];
+       $this->$email = $data["email"];
+       $this->$pseudo = $data["pseudo"];
+       $this->$pswd = $data["pswd"];
+       $this->$city = $data["city"];
+       $this->$street = $data["street"];
+       $this->$number = $data["number"];
+       $this->$rating = $data["rating"];
+       $this->$RoleId = $data["RoleId"];
+    }
+  }
+
+  public static function getAll(){
+    $response = getDB()->query('SELECT * FROM users');
+    $response->setFetchMode(PDO::FETCH_CLASS, 'User');
+    $datas = $response->fetchAll();
+    $response->closeCursor();
+    return $datas;
+  }
+
+  public static function getById($id){
+       $response = getDB()->query('SELECT * FROM users WHERE userId = '. $id);
+       $response->setFetchMode(PDO::FETCH_CLASS, 'User');
+       $data = $response->fetch();
+       $response->closeCursor();
+       return $data;
+   }
+
 
 }
 
