044bca9a03f37c5c72ae66b292d5f4901a0706f5
Runolf <alexandreliskiewicz@hotmail.com>
2020-04-07 19:47:25 +0200

    add code sell view and sell controller

--- project/controllers/sell.php
+++ project/controllers/sell.php
@@ -1,10 +1,18 @@
 <?php
-  require 'models/users.php';
+  require 'models/articles.php';
 
   if(empty($_SESSION['mail'])){
       header("Location: article");
       exit();
   }
+
+
+  if (!empty($_POST)) {
+    if (!empty($_POST['name']) && !empty($_POST['brand']) && !empty($_POST['price']) && !empty($_POST['comment']) && !empty($_POST['picture']) ) {
+      Article::post($_POST['name'], $_POST['brand'], $_POST['picture'], $_POST['price'], $_POST['comment']);
+    }
+  }
+
 
   include 'views/sell.php';
  ?>
--- project/views/sell.php
+++ project/views/sell.php
@@ -2,7 +2,19 @@
   ob_start();
 ?>
 
-<p>Selling things</p>
+<form class="form-group jumbotron" method="post" action="<?=ROOT_PATH.'sell'?>">
+
+  <input type="text" class="form-control" id="brand" name="brand" placeholder="enter the brand of your product"> <br/><br/>
+  <input type="text" class="form-control" id="name" name="name" placeholder="enter the name of your product"> <br/><br/>
+
+  <input type="number" class="form-control" id="price" name="price" placeholder="enter the price of your product"> <br/><br/>
+  <input type="text" class="form-control comment" id="comment" name="comment" placeholder="explain what your product is"> <br/><br/>
+
+  <label for="picture">Enter here the picture of your picture</label>
+  <input type="file" class="form-control" id="picture" name="picture"> <br/><br/>
+
+  <button type="submit" class="btn btn-primary btn-lg" id="button" name="button">Sell</button>
+</form>
 
 <?php
   $title = "sell something";
