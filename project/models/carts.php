6b24b507570cba48f2d7a474b81fee8545267ee6
Runolf <alexandreliskiewicz@hotmail.com>
2020-04-21 16:57:18 +0200

    Update carts.php

--- project/models/carts.php
+++ project/models/carts.php
@@ -18,8 +18,8 @@ class Cart{
   public static function getAll(){
     global $DB;
     try {
-      $response = $DB->query('SELECT * FROM users');
-      $response->setFetchMode(PDO::FETCH_CLASS, 'User');
+      $response = $DB->query('SELECT * FROM carts');
+      $response->setFetchMode(PDO::FETCH_CLASS, 'Cart');
       $datas = $response->fetchAll();
       $response->closeCursor();
 
@@ -27,15 +27,13 @@ class Cart{
     } catch (Exception $e) {
       die('Erreur : ' . $e->getMessage());
     }
-
-
   }
 
   public static function getById($id){
        global $DB;
        try {
-         $response = $DB->query('SELECT * FROM users WHERE userId = '. $id);
-         $response->setFetchMode(PDO::FETCH_CLASS, 'User');
+         $response = $DB->query('SELECT * FROM carts WHERE cartId = '. $id);
+         $response->setFetchMode(PDO::FETCH_CLASS, 'Cart');
          $data = $response->fetch();
          $response->closeCursor();
          return $data;
@@ -43,7 +41,20 @@ class Cart{
        } catch (Exception $e) {
          die('Erreur : ' . $e->getMessage());
        }
+   }
 
+   public static function getCartClient($idUser){
+     global $DB;
+     try {
+       $response = $DB->query('SELECT * FROM carts WHERE userId = '.$idUser);
+       $response->setFetchMode(PDO::FETCH_CLASS, 'Cart');
+       $datas = $response->fetchAll();
+       $response->closeCursor();
+
+       return $datas;
+     } catch (Exception $e) {
+       die('Erreur : ' . $e->getMessage());
+     }
    }
 
    public static function addToCart($_IdArticle, $_IdUser){
@@ -63,59 +74,23 @@ class Cart{
      }
 
    }
-   
-  public static function modify($id , $_mail, $_pseudo, $_pswd, $_city, $_street, $_nbr){
-                            // UPDATE `users` SET rating = 50, RoleId = 2 WHERE userId = 7;
-    global $DB;
 
-    try {
-      $response = $DB->prepare('UPDATE `users` SET email = :email, pseudo = :pseudo,
-        pswd = :pswd,  city = :city, street = :street,  number = :nbr WHERE userId ='.$id);
+   // pas de modify car cela n'a pas de sens de modifier une commande dans ce cas ci.
 
-      $response->setFetchMode(PDO::FETCH_CLASS, 'User');
-
-      if ($_pswd == $_SESSION['pswd']) {
-        $response->execute([':email' => $_mail, ':pseudo' => $_pseudo,
-        ':pswd' => $_SESSION['pswd'] , ':city' => $_city,
-        ':street' => $_street, ':nbr' => $_nbr]);
-      }
-      else {
-        $response->execute([':email' => $_mail, ':pseudo' => $_pseudo,
-        ':pswd' => password_hash($_pswd, PASSWORD_DEFAULT), ':city' => $_city,
-        ':street' => $_street, ':nbr' => $_nbr]);
-      }
-
-
-      $response->closeCursor();
-
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
-
-  }
 
   public static function delete($id){
     global $DB;
 
     try {
-      $deleteSellArticle = $DB->query('DELETE FROM sellarticles WHERE userId = '.$id);
-      $response = $DB->query('DELETE FROM users WHERE userId = '. $id);
+       $DB->query('DELETE FROM carts WHERE cartId = '.$id);
 
-      $response->setFetchMode(PDO::FETCH_CLASS, 'User');
+      $response->setFetchMode(PDO::FETCH_CLASS, 'Cart');
 
-      $deleteSellArticle->closeCursor();
       $response->closeCursor();
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
     }
   }
 
-
-
-  public static function getAllCart($idUser){
-
-  }
-
-}
 
  ?>
