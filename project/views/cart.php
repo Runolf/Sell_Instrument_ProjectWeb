96c3755fa94853970ee9b0bf6f2cabe2aa5e9735
Runolf <alexandreliskiewicz@hotmail.com>
2020-04-22 11:41:46 +0200

    update view cart and add menu for cart

--- project/views/cart.php
+++ project/views/cart.php
@@ -1,11 +1,19 @@
 <?php ob_start() ?>
 
+<div class="container_art">
 <?php foreach($orders as $order):?>
+  <div class="card text-center">
+    <div class="card_title">
+        <?=$order->brand." ".$order->name?>
+    </div>
+    <div class="card_body">
+        <h5>Price : <?= $order->price?> €</h5>
+        <a href="<?=ROOT_PATH.'article/'.$order->articleId?>" class="btn_detail btn btn-primary">Voir le détail</a>
+    </div>
 
-  <p><?= $order->brand ?></p>
-
+  </div>
   <?php endforeach?>
-
+</div>
 <?php
   $title = "My cart";
   $content = ob_get_clean();
--- project/views/includes/template.php
+++ project/views/includes/template.php
@@ -34,6 +34,7 @@
                         }else {
                           echo '<li class="nav-item"><a class="nav-link" href='.ROOT_PATH.'sell>Selling</a></li>';
                           echo '<li class="nav-item"><a class="nav-link" href='.ROOT_PATH.'myArticles>My articles</a></li>';
+                            echo '<li class="nav-item"><a class="nav-link" href='.ROOT_PATH.'cart>My cart</a></li>';
                         }
                         echo '<li class="nav-item"><a class="nav-link" href='.ROOT_PATH.'account>Account</a></li>';
                         echo '<a class="logout btn btn-outline-success my-2 my-sm-0" href="logout.php">logout</a>';
