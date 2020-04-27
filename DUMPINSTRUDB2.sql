a37494bd7eae44cc0edcdd377a4401e9423dd0e4
Runolf <alexandreliskiewicz@hotmail.com>
2020-04-27 15:35:02 +0200

    Update DUMPINSTRUDB2.sql

--- DUMPINSTRUDB2.sql
+++ DUMPINSTRUDB2.sql
@@ -30,6 +30,14 @@ CREATE TABLE `articles` (
   PRIMARY KEY (`articleId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
+/*Data for the table `articles` */
+
+insert  into `articles`(`articleId`,`name`,`brand`,`picture`,`price`,`comment`) values 
+(8,'Zenox','Cort','fehu.png',360.00,'A marvelous guitar'),
+(9,'Test','Terre','deviantart_logo.png',45.00,'jijiji'),
+(10,'Carré','Noix','53049-Wall-Street--Lower-Manhattan.jpg',45.00,'trtr'),
+(11,'Park','Linkin','bibjesus.jpg',47.00,'best band in the world');
+
 /*Table structure for table `carts` */
 
 DROP TABLE IF EXISTS `carts`;
@@ -45,6 +53,12 @@ CREATE TABLE `carts` (
   CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
+/*Data for the table `carts` */
+
+insert  into `carts`(`cartId`,`articleId`,`userId`) values 
+(24,10,11),
+(32,8,13);
+
 /*Table structure for table `roles` */
 
 DROP TABLE IF EXISTS `roles`;
@@ -54,6 +68,12 @@ CREATE TABLE `roles` (
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`roleId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `roles` */
+
+insert  into `roles`(`roleId`,`name`) values 
+(1,'admin'),
+(2,'user');
 
 /*Table structure for table `sellarticles` */
 
@@ -70,6 +90,14 @@ CREATE TABLE `sellarticles` (
   CONSTRAINT `sellarticles_ibfk_1` FOREIGN KEY (`articleId`) REFERENCES `articles` (`articleId`),
   CONSTRAINT `sellarticles_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `sellarticles` */
+
+insert  into `sellarticles`(`sellArticleId`,`articleId`,`userId`,`active`) values 
+(2,8,12,0),
+(3,9,13,1),
+(4,10,13,1),
+(5,11,13,1);
 
 /*Table structure for table `users` */
 
@@ -89,6 +117,13 @@ CREATE TABLE `users` (
   KEY `RoleId` (`RoleId`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`RoleId`) REFERENCES `roles` (`roleId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`userId`,`email`,`pseudo`,`pswd`,`city`,`street`,`number`,`rating`,`RoleId`) values 
+(11,'admin@hotmail.com','admin','$2y$10$qw12qukv99xHmJ2yXOwxyeWm0qTGgSWk4kNYEP6A.YKQtduX.DWya','Le Roeulx','Chaussée de soignies','53B',0.00,1),
+(12,'alexandre@hotmail.com','Alex','$2y$10$7IbHlRredbzC3gCzuWLRg.e3BvFFdS9s46YBgejc4aW1iNg./HPty','Le Roeulx','Chaussée lol','7845',0.00,2),
+(13,'runolf@hotmail.com','Runolf','$2y$10$REH6e3anEBIoQs0nfZ5uG.SU0vAR9WbYqVXsSuFR6mUTJym1IV14G','Kattegat','Regnar','1066',0.00,2);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
